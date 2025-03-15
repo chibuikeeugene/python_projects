ff754f0c7fbc2882db6fd7473f4abc744b951520
Chibuike Iwuchukwu <chibuikeiwuchukwu@Chibuikes-MacBook-Pro.local>
2025-03-15 13:15:57 +0000

    added the next card and flip card logics to the flash card project

--- flash-card-project-start/main.py
+++ flash-card-project-start/main.py
@@ -5,31 +5,34 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 
 data =  pd.read_csv('flash-card-project-start/data/french_words.csv')
+dict_data = data.to_dict(orient='records')
 
-# list_of_data_dict = data.to_dict(orient='records')
-# print(data_dict[0])
+english_word = ''
 
 
 
-# ---------------------------- GENERATE RANDOM WORD ------------------------------- #
-def generate_random_french_word():
-    random_word = random.choice(data['French'])
-    canvas.itemconfig(language_text, text = 'French')
-    canvas.itemconfig(french_word_text, text = random_word)
-    return random_word
+# ---------------------------- GENERATE NEXT CARD LOGIC ------------------------------- #
+def next_card():
+    global english_word
+
+    current_card_data = random.choice(dict_data)
+    
+    english_word = current_card_data['English']
+    
+    canvas.itemconfig(language_text, text = 'French', fill ='black')
+    canvas.itemconfig(word_text, text = current_card_data['French'], fill = 'black')
+    canvas.itemconfig(canvas_image, image = front_img)
+
+    # call the loop that gets the card flipped
+    window.after(3000, func= flip_card)
 
 # ----------------------------- CARD FLIP LOGIC ----------------------------------- #
 
 def flip_card():
     """the logic for card flipping. It provides the English word."""
-    # call the french word generator function
-    french_word =  generate_random_french_word()
-
-    # get its English translation
-    english_word = data[data['French'] == str(french_word)]['English']
 
     # config the canvas text with the new english word
-    canvas.itemconfig(french_word_text, text =  english_word.values[0], fill = 'white')
+    canvas.itemconfig(word_text, text =  english_word, fill = 'white')
 
     # config the canvas language text
     canvas.itemconfig(language_text, text = 'English', fill = 'white')
@@ -37,16 +40,12 @@ def flip_card():
     # config the canvas image
     canvas.itemconfig(canvas_image, image = back_img)
 
-    # window.after_cancel(card_flip_loop)
 
 # ---------------------------- CREATE NEW FLASH CARD UI------------------------------- #
 window = tk.Tk()
 window.title('Flash Card App')
 window.minsize(width=850, height=700)
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
-
-# flip the card after 3 secs
-card_flip_loop = window.after(3000, func= flip_card)
 
 
 # ---------------------------- CREATE CANVAS ------------------------------- #
@@ -57,9 +56,13 @@ front_img =  tk.PhotoImage(file='flash-card-project-start/images/card_front.png'
 back_img = tk.PhotoImage(file='flash-card-project-start/images/card_back.png')
 canvas_image = canvas.create_image(400, 263, image=front_img)
 
+
 # ---------------------------- CREATE TEXT ------------------------------- #
-language_text = canvas.create_text(400, 150, fill= 'black', text='french', font=('Arial', 40, 'italic'))
-french_word_text = canvas.create_text(400, 263, fill= 'black', text='mois', font=('Arial', 60, 'bold'))
+language_text = canvas.create_text(400, 150, fill= 'black', text='', font=('Arial', 40, 'italic'))
+word_text = canvas.create_text(400, 263, fill= 'black', text='', font=('Arial', 60, 'bold'))
+
+# call the first card using the next_card function
+next_card()
 
 
 # ---------------------------- CREATE BUTTONS ------------------------------- #
@@ -70,9 +73,10 @@ check_button = tk.Button(
     bg=BACKGROUND_COLOR,
     highlightthickness=0,
     border=0,
-    command= generate_random_french_word
+    command= next_card
     )
 check_button.grid(row=1, column=0)
+
 
 wrong_img = tk.PhotoImage(file='flash-card-project-start/images/wrong.png')
 cancel_buttton = tk.Button(
@@ -81,7 +85,7 @@ cancel_buttton = tk.Button(
     bg=BACKGROUND_COLOR,
     highlightthickness=0,
     border=0,
-    command=generate_random_french_word
+    command=next_card
     )
 cancel_buttton.grid(row=1, column=1)
 
